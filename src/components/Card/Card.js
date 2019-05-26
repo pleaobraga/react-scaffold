a4d0632386c4d976c8ed339c37e204d13e6f607e
Pedro Leâo <pleao.braga@gmail.com>
2019-05-26 17:27:15 -0300

    add proptypes

---
+++ src/components/Card/Card.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+
+class Card extends React.Component {
+  render() {
+    const { title, text } = this.props
+
+    return (
+      <div className="card" >
+        <hi>{title}</hi>
+        <p>{text}</p>
+      </div>
+    )
+  }
+}
+
+Card.propTypes = {
+  title: PropTypes.string.isRequire,
+  text: PropTypes.string.isRequired
+}
+
