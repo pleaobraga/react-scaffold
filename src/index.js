623cf2da585608d577f6178cda3b1760f7f1a874
Pedro Leâo <pleao.braga@gmail.com>
2019-06-08 17:12:19 -0300

    Create Routers

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import * as React from 'react'
 import ReactDOM from 'react-dom'
 import './styles/_main.scss'
-import WelcomePage from './pages/WelcomePage'
+import Routes from './routes'
 
-ReactDOM.render(<WelcomePage />, document.getElementById('root'))
+ReactDOM.render(<Routes />, document.getElementById('root'))
