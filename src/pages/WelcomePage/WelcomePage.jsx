6d62d95c1f6eaea321c97c21965d28d5e9b6b164
Pedro Leâo <pleao.braga@gmail.com>
2019-06-08 12:01:55 -0300

    Improving lint rules

--- src/pages/WelcomePage/WelcomePage.jsx
+++ src/pages/WelcomePage/WelcomePage.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import { Card } from '../../components'
+import Card from '../../components/Card'
 
 const WelcomePage = () => {
 
   return (
-    <div className='page page--welcome' >
-      <Card title="Welcome" text='This is a rect boilerplate' />
+    <div className="page page--welcome" >
+      <Card title="Welcome" text="This is a rect boilerplate" />
     </div>
   )
 
