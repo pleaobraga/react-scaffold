309121194add389416dc23f3ab9c8cc9bf56440d
Pedro Leâo <pleao.braga@gmail.com>
2019-06-18 22:50:16 -0300

    Improve ContentPage tests

--- src/pages/ContentPage/ContentPage.test.js
+++ src/pages/ContentPage/ContentPage.test.js
@@ -6,49 +6,46 @@ import ContentPage from './ContentPage'
 import { Provider } from 'react-redux'
 import { mockStore, initialState } from '../../reducers/__mocks__/reduxMock'
 
+const setup = state => {
+  const newState = {
+    ...initialState,
+    ...state
+  }
+
+  const store = mockStore(newState)
+
+  const contentPage = mount(
+    <Provider store={store}>
+      <ContentPage />
+    </Provider>
+  )
+
+  return contentPage
+}
+
 describe('Content Page', () => {
   it('should render loading correctly', () => {
-    const store = mockStore(initialState)
-
-    const contentPage = mount(
-      <Provider store={store}>
-        <ContentPage />
-      </Provider>
-    )
+    const contentPage = setup()
 
     expect(contentPage).toMatchSnapshot()
   })
 
   it('should render content correctly', () => {
     const newState = {
-      ...initialState,
       content: { text: 'text', title: 'title' }
     }
 
-    const store = mockStore(newState)
-
-    const contentPage = mount(
-      <Provider store={store}>
-        <ContentPage />
-      </Provider>
-    )
+    const contentPage = setup(newState)
 
     expect(contentPage).toMatchSnapshot()
   })
 
   it('should render error correctly', () => {
     const newState = {
-      ...initialState,
       errorContent: true
     }
 
-    const store = mockStore(newState)
-
-    const contentPage = mount(
-      <Provider store={store}>
-        <ContentPage />
-      </Provider>
-    )
+    const contentPage = setup(newState)
 
     expect(contentPage).toMatchSnapshot()
   })
