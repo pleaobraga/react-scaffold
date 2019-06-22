4195ab26790eb14796483a711e8c4bb59a934d23
Pedro Leâo <pleao.braga@gmail.com>
2019-06-22 09:39:15 -0300

    Fixed web pack prod

--- webpack.prod.js
+++ webpack.prod.js
@@ -5,10 +5,10 @@ const common = require('./webpack.common.js')
 module.exports = merge(common, {
   mode: 'production',
   devtool: 'source-map',
-  
+
   output: {
-      path: path.resolve(__dirname, 'dist'),
-      filename: '[name].[hash:8].js',
-      chunkFilename: '[name].[hash:8].chunk.js'
-    }
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].[hash:8].js',
+    chunkFilename: '[name].[hash:8].chunk.js'
+  }
 })
