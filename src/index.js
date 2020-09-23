50d3c3b20ffa57ac229f78b2e7a7d65b7caa1219
pleaobraga <pleao.braga@gmail.com>
2020-09-23 07:04:57 -0300

    Update React import

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import * as React from 'react'
+import React from 'react'
 import ReactDOM from 'react-dom'
 import { createStore, applyMiddleware, compose } from 'redux'
 import { Provider } from 'react-redux'
